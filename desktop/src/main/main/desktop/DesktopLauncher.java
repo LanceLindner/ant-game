7ffa2a51de71d06d79359183b680d734eeca1bb8
LanceLindner <lance.j.lindner@gmail.com>
2019-05-11 14:17:40 -0500

    configure desktop window default size and position

--- desktop/src/main/main/desktop/DesktopLauncher.java
+++ desktop/src/main/main/desktop/DesktopLauncher.java
@@ -7,6 +7,10 @@ import main.main.Main;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.x=0;
+		config.y=0;
+		config.width=880;
+		config.height=880;
 		new LwjglApplication(new Main(), config);
 	}
 }

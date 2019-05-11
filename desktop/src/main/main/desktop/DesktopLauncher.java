d2be786640d916c8b955cc638adf91c5a92f7c38
LanceLindner <lance.j.lindner@gmail.com>
2019-05-11 14:28:33 -0500

    Reconfigure files to accomodate TextureAtlas

--- desktop/src/main/main/desktop/DesktopLauncher.java
+++ desktop/src/main/main/desktop/DesktopLauncher.java
@@ -2,15 +2,18 @@ package main.main.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+
 import main.main.Main;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.x=0;
-		config.y=0;
-		config.width=880;
-		config.height=880;
+		config.x = 0;
+		config.y = 0;
+		config.width = 880;
+		config.height = 880;
+		TexturePacker.process("assets/sprites/sprites", "assets/sprites/atlas", "atlas");
 		new LwjglApplication(new Main(), config);
 	}
 }

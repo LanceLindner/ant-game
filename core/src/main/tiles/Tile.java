60b9d5d6ff64b7f55a36ee4640c8d3806a01e145
LanceLindner <lance.j.lindner@gmail.com>
2019-05-11 22:29:37 -0500

    Add the entity field and related methods to the the Tile class

--- core/src/main/tiles/Tile.java
+++ core/src/main/tiles/Tile.java
@@ -1,5 +1,6 @@
 package main.tiles;
 
+import main.entities.Entity;
 import main.world.World;
 
 public class Tile {
@@ -7,9 +8,23 @@ public class Tile {
 	private int x;
 	private int y;
 
+	private Entity entity;
+
 	private double visibility;
 
 	public Tile() {
 
+	}
+
+	public boolean isThereAnEntityOnThisTile() {
+		return entity != null ? true : false;
+	}
+
+	public void addEntity(Entity newEntity) {
+		entity = newEntity;
+	}
+
+	public void removeEntity() {
+		entity = null;
 	}
 }

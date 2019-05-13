953ef5b91b0b841a737f706258596f8a3249ff2b
Daniel Dyer <danielmdyer@gmail.com>
2019-05-13 17:37:17 -0500

    Created Ant and Brain
    Created the very rudimentary version of Ant and Brain. Still requires a lot of work.

---
+++ core/src/main/entities/ant/Ant.java
@@ -0,0 +1,24 @@
+package main.entities.ant;
+
+public class Ant {
+
+	private Brain brain;
+	private int health;
+	private int direction;
+
+	public Ant() {
+
+	}
+
+	public void move() {
+		int out[] = brain.getOutput();
+	}
+
+	private void turnLeft() {
+		direction = (direction - 1) % 4;
+	}
+
+	private void turnRight() {
+		direction = (direction + 1) % 4;
+	}
+}
---
+++ core/src/main/entities/ant/Brain.java
@@ -0,0 +1,20 @@
+package main.entities.ant;
+
+public class Brain {
+
+	private int[] input;
+	private int[] output;
+
+	public Brain(int input, int output) {
+		this.input = new int[input];
+		this.output = new int[output];
+	}
+
+	public void update(int[] input) {
+
+	}
+
+	protected int[] getOutput() {
+		return output;
+	}
+}

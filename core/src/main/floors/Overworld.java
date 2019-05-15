17480bc8d5062e588db5639df837fffa180fbeed
LanceLindner <lance.j.lindner@gmail.com>
2019-05-15 15:55:33 -0500

    Add test ants to the overworld and and ant texture

--- core/src/main/floors/Overworld.java
+++ core/src/main/floors/Overworld.java
@@ -1,12 +1,14 @@
 package main.floors;
 
 import main.entities.TestEntity;
+import main.entities.ants.Ant;
 
 public class Overworld extends Floor {
 	public Overworld() {
 		super("overworld");
 		for (int i = 0; i < 50; ++i) {
 			entities.add(new TestEntity(0, 0));
+			entities.add(new Ant(495, 495));
 		}
 	}
 

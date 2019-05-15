b33655b50335799ba64049df77943e2e536f3e2e
LanceLindner <lance.j.lindner@gmail.com>
2019-05-15 13:56:36 -0500

    Fill in Neuron.java and Axon.java

--- core/src/main/entities/ants/Axon.java
+++ core/src/main/entities/ants/Axon.java
@@ -1,5 +1,33 @@
 package main.entities.ants;
 
 public class Axon {
+	public Neuron inputNeuron;
+	public Neuron outputNeuron;
 
+	public double weight;
+
+	public Axon(Neuron inputNeuron, Neuron outputNeuron) {
+		this.inputNeuron = inputNeuron;
+		this.outputNeuron = outputNeuron;
+		randomizeWeight();
+	}
+
+	public Axon(Neuron inputNeuron, Neuron outputNeuron, double weight) {
+		this.inputNeuron = inputNeuron;
+		this.outputNeuron = outputNeuron;
+		this.weight = weight;
+	}
+
+	public void delete() {
+		inputNeuron.outputAxons.remove(this);
+		outputNeuron.inputAxons.remove(this);
+	}
+
+	public void randomizeWeight() {
+		weight = (int) Math.round((Math.random() * 2 - 1) * Math.pow(10, 1)) / Math.pow(10, 1);
+	}
+
+	public void setOutputNeuronValue(double value) {
+		outputNeuron.changeValue(value * weight);
+	}
 }
--- core/src/main/entities/ants/Neuron.java
+++ core/src/main/entities/ants/Neuron.java
@@ -1,5 +1,58 @@
 package main.entities.ants;
 
-public class Neuron {
+import java.util.ArrayList;
 
+public class Neuron {
+	public double value = 0;
+	public boolean active = false;
+
+	public ArrayList<Axon> inputAxons = new ArrayList<Axon>();
+	public ArrayList<Axon> outputAxons = new ArrayList<Axon>();
+
+	public double x;
+	public double y;
+
+	public Neuron childNeuron;// used for cloning
+
+	public Neuron() {
+	}
+
+	public void delete() {
+		for (Axon axon : inputAxons) {
+			axon.delete();
+		}
+		for (Axon axon : outputAxons) {
+			axon.delete();
+		}
+	}
+
+	public double getValue() {
+		if (value > 1)
+			return 1;
+		else if (value < 0)
+			return 0;
+		else
+			return value;
+
+	}
+
+	private void updateOutputAxons(double deltaValue) {
+		for (Axon axon : outputAxons) {
+			axon.setOutputNeuronValue(deltaValue);
+		}
+	}
+
+	public void setValue(double newValue) {
+		newValue = (int) Math.round(newValue * Math.pow(10, 1)) / Math.pow(10, 1);
+		if (Math.abs(newValue - value) > 0.05) {
+			double deltaValue = newValue - value;
+			value = newValue;
+			updateOutputAxons(deltaValue);
+		}
+	}
+
+	public void changeValue(double deltaValue) {
+		value += deltaValue;
+		updateOutputAxons(deltaValue);
+	}
 }

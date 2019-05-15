96b1ce5acadc1dd5082c95ae7bb60c171fc0b96b
LanceLindner <lance.j.lindner@gmail.com>
2019-05-15 15:25:10 -0500

    Begin connecting Neurons to the Brain

--- core/src/main/entities/brain/Brain.java
+++ core/src/main/entities/brain/Brain.java
@@ -1,20 +1,45 @@
 package main.entities.brain;
 
+import java.util.ArrayList;
+
+import main.entities.ants.Neuron;
+
 public class Brain {
+	private Neuron[] input;
+	private Neuron[] hidden;
+	private Neuron[] output;
 
-	private int[] input;
-	private int[] output;
+	public ArrayList<Neuron> allNeurons = new ArrayList<Neuron>();
+	public ArrayList<Neuron> activeInput = new ArrayList<Neuron>();
 
-	public Brain(int input, int output) {
-		this.input = new int[input];
-		this.output = new int[output];
+	public Brain(int numberOfInputNeurons, int numberOfHiddenNeurons, int numberOfOutputNeurons) {
+		initializeEmptyBrain(numberOfInputNeurons, numberOfHiddenNeurons, numberOfOutputNeurons);
 	}
 
 	public void update(int[] input) {
 
 	}
 
+	public void initializeEmptyBrain(int numberOfInputNeurons, int numberOfHiddenNeurons, int numberOfOutputNeurons) {
+		input = new Neuron[numberOfInputNeurons];
+		hidden = new Neuron[numberOfHiddenNeurons];
+		output = new Neuron[numberOfOutputNeurons];
+
+		for (Neuron neuron : input) {
+			neuron = new Neuron();
+			allNeurons.add(neuron);
+		}
+		for (Neuron neuron : hidden) {
+			neuron = new Neuron();
+			allNeurons.add(neuron);
+		}
+		for (Neuron neuron : output) {
+			neuron = new Neuron();
+			allNeurons.add(neuron);
+		}
+	}
+
 	public int[] getOutput() {
-		return output;
+		return null;
 	}
 }

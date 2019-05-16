74bb177f0a79d0ffcfafe9367ab1c08ac4e26f71
LanceLindner <lance.j.lindner@gmail.com>
2019-05-15 23:31:28 -0500

    Document the Neuron class

--- core/src/main/entities/brain/Neuron.java
+++ core/src/main/entities/brain/Neuron.java
@@ -8,9 +8,18 @@ public class Neuron {
 
 	private ArrayList<Integer> outputNeuronIndexes = new ArrayList<Integer>();
 
+	/**
+	 * Constructor for the neuron
+	 */
 	public Neuron() {
 	}
 
+	/**
+	 * Returns this neuron's value, adjusted to fit within the bounds of 0 and 1.
+	 * These bounds are present because the actual value needs a larger range.
+	 *
+	 * @return this neuron's value, adjusted to fit within the bounds of 0 and 1
+	 */
 	public double getValue() {
 		if (value > 1)
 			return 1;
@@ -20,18 +29,48 @@ public class Neuron {
 			return value;
 	}
 
+	/**
+	 * Returns this neuron's ArrayList of output neuron indexes. This should be
+	 * called by the brain when this neuron's setValue() method returns a value that
+	 * indicated the output neurons need to be updated
+	 *
+	 * @return an ArraList of this neuron's output neurons indexes
+	 */
 	public ArrayList<Integer> getOutputNeuronIndexes() {
 		return outputNeuronIndexes;
 	}
 
-	public void addOutputNeuronIndex(int axonIndex) {
-		outputNeuronIndexes.add(axonIndex);
+	/**
+	 * Adds an output neuron index to the list of neurons to change when this
+	 * neuron's values are changed
+	 *
+	 * @param outputNeuronIndex outputNeuronIndex the index of the output neurons
+	 *                          that should be added
+	 */
+	public void addOutputNeuronIndex(int outputNeuronIndex) {
+		outputNeuronIndexes.add(outputNeuronIndex);
 	}
 
-	public void removeOutputNeuronIndex(int axonIndex) {
-		outputNeuronIndexes.remove(axonIndex);
+	/**
+	 * Remove the index of a given output neuron from the list of neurons to change
+	 * when this neuron's values are changed
+	 *
+	 * @param outputNeuronIndex the index of the output neurons that should be
+	 *                          removed
+	 */
+	public void removeOutputNeuronIndex(int outputNeuronIndex) {
+		outputNeuronIndexes.remove(outputNeuronIndex);
 	}
 
+	/**
+	 * If this neuron is active and the difference between the two is greater than
+	 * 10%, this neurons value is set to the parameter's value and the difference is
+	 * returned so that the brain can change all output neurons
+	 *
+	 * @param newValue the value that this neuron's value will be set to if the
+	 *                 difference between the two is greater than 10%
+	 * @return how much all output neurons should change
+	 */
 	public double setValue(double newValue) {
 		if (active == true) {
 			newValue = (int) Math.round(newValue * Math.pow(10, 1)) / Math.pow(10, 1);

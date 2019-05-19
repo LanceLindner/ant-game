bc606fa0beb0906ce55c7a586826fdedbda704bc
LanceLindner <lance.j.lindner@gmail.com>
2019-05-19 17:48:45 -0500

    Create Constructor and general fields for SpriteSheet.java

--- core/src/main/SpriteSheets/SpriteSheet.java
+++ core/src/main/SpriteSheets/SpriteSheet.java
@@ -1,5 +1,26 @@
 package main.SpriteSheets;
 
-public class SpriteSheet {
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Animation.PlayMode;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
+import main.main.Globals;
+
+public class SpriteSheet {
+	private static final float FRAME_DURATION = 1 / 60f;
+
+	private float stateBegin = 0;
+	private Animation<TextureRegion> active;
+	private Animation<TextureRegion> idle, walk;
+
+	public TextureRegion lastFrame;
+
+	public SpriteSheet(String name) {
+		TextureAtlas atlas = Globals.assetManagerManager.getTextureAtlas();
+		idle = new Animation<TextureRegion>(FRAME_DURATION, atlas.findRegions(name + "Idle"), PlayMode.LOOP);
+		walk = new Animation<TextureRegion>(FRAME_DURATION, atlas.findRegions(name + "Walk"), PlayMode.LOOP);
+
+		active = walk;
+	}
 }

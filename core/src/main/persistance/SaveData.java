252306899c2264696914c621f2af6f0ac1327e93
LanceLindner <lance.j.lindner@gmail.com>
2019-05-20 16:10:58 -0500

    Alter SaveData to manage saved ants

--- core/src/main/persistance/SaveData.java
+++ core/src/main/persistance/SaveData.java
@@ -2,10 +2,12 @@ package main.persistance;
 
 import java.util.ArrayList;
 
+import main.entities.ants.Ant;
 import main.main.Globals;
 
 public class SaveData {
 	private ArrayList<boolean[]> unlockedNeurons = new ArrayList<boolean[]>();
+	private ArrayList<Ant> savedAnts = new ArrayList<Ant>();
 
 	public SaveData() {
 		unlockedNeurons.add(new boolean[Globals.NUMBER_OF_INPUT_NEURONS]);
@@ -17,9 +19,19 @@ public class SaveData {
 				unlockedNeurons.get(i)[j] = false;
 			}
 		}
+
+		savedAnts = new ArrayList<Ant>();
 	}
 
 	public void unlockNeuron(int x, int y) {
 		unlockedNeurons.get(y)[x] = true;
+	}
+
+	public void saveAnt(Ant ant) {
+		savedAnts.add(ant);
+	}
+
+	public void deleteAnt(Ant ant) {
+		savedAnts.remove(ant);
 	}
 }

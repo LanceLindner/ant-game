577ea890b7988fd18375d96b36e8417a9a09c956
LanceLindner <lance.j.lindner@gmail.com>
2019-05-20 15:09:41 -0500

    Create Constructor for SaveData

--- core/src/main/persistance/SaveData.java
+++ core/src/main/persistance/SaveData.java
@@ -1,5 +1,21 @@
 package main.persistance;
 
-public class SaveData {
+import java.util.ArrayList;
 
+import main.main.Globals;
+
+public class SaveData {
+	private ArrayList<boolean[]> unlockedNeurons = new ArrayList<boolean[]>();
+
+	public SaveData() {
+		unlockedNeurons.add(new boolean[Globals.NUMBER_OF_INPUT_NEURONS]);
+		unlockedNeurons.add(new boolean[Globals.NUMBER_OF_HIDDEN_NEURONS]);
+		unlockedNeurons.add(new boolean[Globals.NUMBER_OF_OUTPUT_NEURONS]);
+
+		for (int i = 0; i < unlockedNeurons.size(); ++i) {
+			for (int j = 0; j < unlockedNeurons.get(i).length; ++j) {
+				unlockedNeurons.get(i)[j] = false;
+			}
+		}
+	}
 }

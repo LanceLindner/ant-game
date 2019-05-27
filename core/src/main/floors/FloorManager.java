465057a736fc3bbd28d80ae97b4cd0cd94092dab
LanceLindner <lance.j.lindner@gmail.com>
2019-05-27 17:39:41 -0500

    Add addFloor to floorManager

--- core/src/main/floors/FloorManager.java
+++ core/src/main/floors/FloorManager.java
@@ -3,6 +3,10 @@ package main.floors;
 import java.util.HashMap;
 
 public class FloorManager {
-	private static HashMap<Integer, Floor> allFloors;
+	private static HashMap<String, Floor> allFloors = new HashMap<String, Floor>();
 	private Floor currentFloor;
+
+	public void addFloor(Floor floor) {
+		allFloors.put(floor.getName(), floor);
+	}
 }
